be78e4318cf6bd5d7a6d8946dcd854bfb30114e6
Amidgard <44028334+Frussia122@users.noreply.github.com>
2023-01-22 16:49:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,69 +1,73 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
-    '**********': ' ',
+  '.-':     'a',
+  '-...':   'b',
+  '-.-.':   'c',
+  '-..':    'd',
+  '.':      'e',
+  '..-.':   'f',
+  '--.':    'g',
+  '....':   'h',
+  '..':     'i',
+  '.---':   'j',
+  '-.-':    'k',
+  '.-..':   'l',
+  '--':     'm',
+  '-.':     'n',
+  '---':    'o',
+  '.--.':   'p',
+  '--.-':   'q',
+  '.-.':    'r',
+  '...':    's',
+  '-':      't',
+  '..-':    'u',
+  '...-':   'v',
+  '.--':    'w',
+  '-..-':   'x',
+  '-.--':   'y',
+  '--..':   'z',
+  '.----':  '1',
+  '..---':  '2',
+  '...--':  '3',
+  '....-':  '4',
+  '.....':  '5',
+  '-....':  '6',
+  '--...':  '7',
+  '---..':  '8',
+  '----.':  '9',
+  '-----':  '0',
+  '**********': ' ',
 };
 
 function decode(expr) {
-    let keys = Object.keys(MORSE_TABLE),
-        values = Object.values(MORSE_TABLE);
-    let input = expr.match(/\S{10}/ig);
- 
-  
-    let rightInput = input.map(item => {
-      let index = item.split("").findIndex((el)=> el === "1");
-      if(index >= 0) return item.split("").slice(index);
-      return item
-    });
-  
-     rightInput = rightInput.map(item => {
-       if(item != "**********") {
-       let morse = item.join("").match(/\S{2}/ig);
-         return morse.map(element => {return element.replaceAll(/10/g, '.').replaceAll(/11/g, '-')}).join('');
-       } else return item
-      })
+  let keys = Object.keys(MORSE_TABLE),
+      values = Object.values(MORSE_TABLE);
+  let input = expr.match(/\S{10}/ig);
 
-    let answer = rightInput.map(item => {
-      for(i=0; i<keys.length; i++) {
-        if(item === keys[i]) {
-          return values[i];
-        }
-      }
+
+  let rightInput = input.map(item => {
+    let index = item.split("").findIndex((el)=> el === "1");
+    if(index >= 0) return item.split("").slice(index);
+    return item
+  });
+
+   rightInput = rightInput.map(item => {
+     if(item != "**********") {
+     let morse = item.join("").match(/\S{2}/ig);
+       return morse.map(element => {return element.replaceAll(/10/g, '.').replaceAll(/11/g, '-')}).join('');
+     } else return item
     })
-    
-    return answer.join('')
+
+  let answer = rightInput.map(item => {
+    for(i=0; i<keys.length; i++) {
+      if(item === keys[i]) {
+        return values[i];
+      }
+    }
+  })
+  return answer.join('')
+}
+
+
+module.exports = {
+    decode
 }
